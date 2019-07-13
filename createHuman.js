35a7024fdea95e424ac313f3d232828c7d124ee3
alekiko72 <alekiko72@users.noreply.github.com>
2019-07-13 00:01:20 -0400

    Complete Create Human JS

--- createHuman.js
+++ createHuman.js
@@ -1,6 +1,7 @@
-var stats = [20];
-var skills = [30];
-var talents = [30]
+var stats = [];
+var skills = [];
+var talents = [];
+var trappings = [];
 var d1;
 var d2;
 var tot;
@@ -33,8 +34,11 @@ function createHum(){
     console.log(career);
     console.log(skills);
     skills = skills.concat(getCarSki(career));
+    talents = talents.concat(getCarTal(career));
+    trappings = trappings.concat(getCarTrap(career));
     console.log(skills);
     console.log(talents);
+    console.log(trappings);
 }
 
 function getBaseStats(){
@@ -169,7 +173,7 @@ function randTalents(){
 }
 
 function chooseHumCar(){
-    var dCar = Math.floor(Math.random() * (+101 - +1));
+    var dCar = Math.floor(Math.random() * (+101 - +1))+ +1;
 
     switch(dCar){
         case 1 :
@@ -202,7 +206,7 @@ function chooseHumCar(){
             return "Camp Follower";
         case 19 :
         case 20 :
-            return "Charcoal Burner";
+            return "Charcoal-Burner";
         case 21 :
         case 22 :
             return "Coachman";
@@ -542,61 +546,353 @@ function getCarTal(car){
             var bodyguard = ["Disarm", "Quick Draw", "Specialist Weapon Group(Parrying)", "Specialist Weapon Group(Throwing)",];
             return bodyguard;
         case "Bone Picker" :
+            var bone = ["Coolheaded","Streetwise", "Hardy", "Resistance to Disease"];
+            return bone;
         case "Bounty Hunter" :
+            var bounty = ["Marksman", "Strike to Stun", "Rover", "Specialist Weapon Group(Entangling)", "Sharpshooter", "Strike Mighty Blow"];
+            return bounty;
         case "Burgher" :
+            var burg = [ "Dealmaker", "Savvy", "Suave"];
+            return burg;
         case "Camp Follower" :
+            var camp = [ "Dealmaker", "Street Fighter", "Flee!", "Hardy", "Suave", "Resistance to Disease", "Seasoned Traveller"];
+            return camp;
         case "Charcoal-Burner" :
+            var charcoal = ["Savvy", "Very Strong"];
+            return charcoal; 
         case "Coachman" :
+            var coach = ["Quick Draw", "Seasoned Traveller", "Specialist Weapon Group(Gunpowder)"];
+            return coach;
         case "Entertainer" :
+            var entertain = ["Contortionist", "Lightning Reflexes", "Mimic", "Public Speaking", "Quick Draw", "Sharpshooter", "Specialist Weapon Group(Throwing)", "Trick Riding", "Very Strong", "Wrestling"];
+            return entertain;
         case "Envoy" :
+            var envoy = ["Deal Maker", "Seasoned Traveller"];
+            return envoy;
         case "Estalian Diestro" :
+            var estal = ["Lightning Reflexes", "Swashbuckler", "Quick Draw", "Strike to Injure", "Specialist Weapon Group(Fencing)", "Strike Mighty Blow"]
+            return estal;
         case "Ferryman" :
+            var ferry = ["Marksman", "Suave", "Specialist Weapon Group(Gunpowder)", "Street Fighting"];
+            return ferry; 
         case "Fieldwarden" :
+            var field = ["Fleet Footed", "Savvy", "Mighty Shot", "Rapid Reload", "Rover", "Quick Draw"];
+            return field;
         case "Fisherman" :
-        case "Grave Robber" :
+            var fisher = [ "Hardy", "Savvy", "Orientation", "Street Fighting"];
+            return fisher;
+        case "Grave Robber" : 
+            var grave = ["Flee!", "Resistance to Disease", "Streetwise", "Strongminded"];
+            return grave;
         case "Hedge Wizard" :
+            var hedge = ["Hedge Magic", "Petty Magic(Hedge)"];
+            return hedge;
         case "Hunter" :
+            var hunt = ["Hardy", "Specialist Weapon Group(Longbow)", "Lightning Reflexes", "Very Resilient", "Marksman", "Rover", "Rapid Reload"];
+            return hunt;
         case "Initiate" :
+            var init = ["Lightning Reflexes", "Very Strong", "Public Speaking", "Suave", "Warrior Born"];
+            return init;
         case "Jailer" :
+            var jail = ["Resistance to Disease", "Resistance to Poison", "Specialist Weapon Group(Entangling)", "Wrestling"];
+            return jail; 
         case "Kislevite Kossar" :
+            var kislev = ["Specialist Weapon Group(Two-handed)", "Strike to Injure"];
+            return kislev;
         case "Kithband Warrior" :
+            var kith = [ "Marksman", "Rover", "Rapid Reload", "Warrior Born"];
+            return kith;
         case "Marine" :
+            var marine = ["Disarm", "Quick Draw", "Strike Mighty Blow", "Strike to Stun"];
+            return marine;
         case "Mercenary" :
-        case "Messenger" :
+            var merc = ["Disarm", "Quick Draw", "Rapid Reload", "Strike Mighty Blow", "Sharpshooter", "Strike to Stun"];
+            return merc;
+        case "Messenger" : 
+            var mess = ["Orientation", "Seasoned Traveller"];
+            return mess;
         case "Militiaman" :
+            var militia = ["Specialist Weapon Group(Two-handed)", "Rapid Reload", "Strike Mighty Blow"];
+            return militia;
         case "Miner" :
+            var miner = ["Orientation", "Specialist Weapon Group(Two-handed)", "Very Resilient", "Warrior Born"];
+            return miner;
         case "Noble" :
+            var noble = ["Etiquette", "Luck", "Public Speaking", "Savvy", "Specialist Weapon(Fencing)", "Schemer", "Specialist Weapon(Parrying)"];
+            return noble;
         case "Norse Berserker" :
+            var norse = ["Frenzy", "Menacing", "Quick Draw", "Specialist Weapon Group(Two-handed)"];
+            return norse;
         case "Outlaw" :
+            var outlaw = [ "Rover", "Streetwise", "Sharpshooter", "Strike to Stun"];
+            return outlaw;
         case "Outrider" :
+            var outrider = ["Coolheaded", "Very Strong", "Orientation", "Specialist Weapon Group(Entangling)"];
+            return outrider;
         case "Peasant" :
+            var peasant = ["Hardy", "Rover", "Flee!", "Specialist Weapon Group(Sling)"];
+            return peasant; 
         case "Pit Fighter" :
+            var pit = ["Disarm", "Wrestling", "Quick Draw", "Strike to Injure", "Specialist Weapon Group(Flail)", "Specialist Weapon Group(Parrying)", "Specialist Weapon Group(Two-handed)", "Strike Mighty Blow", "Very Strong", "Strong-minded"];
+            return pit;
         case "Protagonist" :
+            var protag = ["Disarm", "Quick Draw", "Menacing", "Suave", "Street Fighting", "Strike Mighty Blow", "Strike to Injure", "Strike to Stun"];
+            return protag;
         case "Rat Catcher" :
+            var rat = ["Resistance to Disease", "Resistance to Poison", "Specialist Weapon Group(Sling)", "Tunnel Rat"];
+            return rat;
         case "Roadwarden" :
+            var road = ["Quick Draw", "Rapid Reload", "Specialist Weapons Group(Gunpowder)"];
+            return road;
         case "Rogue" :
+            var rogue = ["Flee!", "Streetwise", "Luck", "Sixth Sense", "Public Speaking"];
+            return rogue;
         case "Runebearer" :
+            var rune = ["Flee!", "Fleet Footed", "Sixth Sense", "Orientation", "Rapid Reload", "Very Resilient", "Very Strong"];
+            return rune; 
         case "Scribe" :
+            var scribe = ["Linguistics"];
+            return scribe; 
         case "Seaman" :
+            var sea = [ "Hardy", "Street Fighting", "Seasoned Traveller", "Strike Mighty Blow", "Swashbuckler"];
+            return sea; 
         case "Servant" :
+            var serve = ["Acute Hearing", "Flee!", "Etiquette", "Hardy", "Lightning Reflexes", "Very Resilient"];
+            return serve;
         case "Shieldbreaker" :
+            var shield = ["Acute Hearing", "Flee!", "Etiquette", "Hardy", "Lightning Reflexes", "Very Resilient"];
+            return shield;
         case "Smuggler" :
+            var smug = ["Dealmaker", "Streetwise"];
+            return smug;
         case "Soldier" :
+            var soldider = ["Disarm", "Quick Draw", "Sharpshooter", "Strike Mighty Blow", "Specialist Weapon Group(Gunpowder)", "Specialist Weapon Group(Two-handed)", "Strike to Injure", "Rapid Reload", "Strike to Stun", "Mighty Shot"]
         case "Squire" :
+            var squire = ["Etiquette", "Specialist Weapon Group(Cavalry)", "Strike Mighty Blow"];
+            return squire; 
         case "Student" :
+            var student = ["Etiquette", "Linguistics", "Savvy", "Suave", "Seasoned Traveller", "Super Numerate"];
+            return student;
         case "Thief" :
+            var thief = ["Alley Cat", "Streetwise", "Super Numerate", "Trapfinder"];
+            return thief;
         case "Thug" :
+            var thug = ["Coolheaded", "Lightning Reflexes", "Disarm", "Resistance to Poison", "Quick Draw", "Strike to Injure", "Wrestling", "Strike to Stun"];
+            return thug;
         case "Toll Keeper" :
+            var toll = ["Lightning Reflexes", "Marksman"];
+            return toll;
         case "Tomb Robber" :
+            var tomb = [ "Luck", "Sixth Sense", "Trapfinder", "Tunnel Rat"];
+            return tomb;
         case  "Tradesman" :
+            var trades = ["Dealmaker", "Savvy"];
+            return trades;
         case "Troll Slayer" :
+            var troll = ["Disarm", "Quick Draw", "Hardy", "Lightning Reflexes", "Very Resilient", "Specialist Weapon Group(Two-handed)", "Street Fighter", "Strike Mighty Blow"];
+            return troll;
         case "Vagabond" :
+            var vaga = ["Fleet Footed", "Rover", "Marksman", "Orientation", "Seasoned Traveller"];
+            return vaga;
         case "Valet" :
+            var valet = ["Coolheaded", "Suave", "Dealmaker", "Seasoned Traveller", "Etiquette"];
+            return valet;
         case "Watchman" :
+            var watch = ["Coolheaded", "Savvy", "Disarm", "Street Fighting", "Strike Mighty Blow", "Strike to Stun"];
+            return watch; 
         case "Woodsman" :
+            var woods = ["Fleet Footed", "Very Resilient", "Rover", "Specialist Weapon Group(Two-handed)"];
+            return woods; 
         case "Zealot" :
+            var zealot = ["Coolheaded", "Very Strong", "Hardy", "Suave", "Public Speaking", "Specialist Weapon Group(Flail)"];
+            return zealot;
     }
 
 
+
+}
+
+function getCarTrap(car){
+    switch(car){
+        case "Agitator" :
+            var agitator = ["Leather Jack", "One set of Good Craftsmenship Clothes", "20 leaflets for various causes"]; 
+            return agitator;
+        case "Apprentice Wizard" :
+            var apprentice = ["Quarter Staff", "Backpack", "Printed Book"];
+            return apprentice;
+        case "Bailiff" :
+            var bailiff = ["Leather Jack", "Leather Skullcap", "Riding Horse", "Saddle", "Harness", "One Set of Good Craftsmenship Clothing"];
+            return bailiff;
+        case  "Barber-Surgeon" :
+            var barber = ["Trade Tools (Barber-Surgeon)"];
+            return barber;
+        case "Boatman" :
+            var boatman = ["Leather Jack", "Row Boat"];
+            return boatman;
+        case "Bodyguard" :
+            var bodyguard = ["Buckler", "Knuckle-dusters", "A Pair of Throwing Axes", "Throwing Knives", "Leather Jack"];
+            return bodyguard;
+        case "Bone Picker" :
+            var bone = ["Cart", "3 Sacks"];
+            return bone;
+        case "Bounty Hunter" :
+            var bounty = ["Crossbow", "10 bolts", "Net", "Leather Jerkin", "Leather Skullcap", "Manacles", "10 Yards of Rope"];
+            return bounty;
+        case "Burgher" :
+            var burg = ["Abacus", "Lantern", "One Set of Good Clothing"];
+            return burg;
+        case "Camp Follower" :
+            var camp = ["Lucky Charm", "Trade Tools", "Pouch", "Tent"];
+            return camp;
+        case "Charcoal-Burner" :
+            var charcoal = ["3 Torches", "Tinderbox", "Hatchet"];
+            return charcoal; 
+        case "Coachman" :
+            var coach = ["Blunderbuss", "Powder/ammunition for 10 shots", "Mail Shirt", "Leather Jack", "Instrument (Coach Horn)"];
+            return coach;
+        case "Entertainer" :
+            var entertain = ["Leather Jerkin", "Instrument (any)", "Trade Tools (Performer)", "3 Throwing Knives", "2 Throwing Axes", "Costume", "One Set of Good Craftsmenship Clothes"];
+            return entertain;
+        case "Envoy" :
+            var envoy = ["Leather Jack", "2 sets of Good Craftsmenship Clothes", "Writing Kit"];
+            return envoy;
+        case "Estalian Diestro" :
+            var estal = ["Foil", "Rapier", "One set of Best Craftsmenship Clothes", "Perfume", "Cologne", "Healing Draught"]
+            return estal;
+        case "Ferryman" :
+            var ferry = ["Crossbow", "10 bolts", "Blunderbuss", "Powder/ammunition of 10 shots", "Leather Jack"];
+            return ferry; 
+        case "Fieldwarden" :
+            var field = [ "Sling with Ammunition", "Lantern", "Lamp Oil", "Spade", "Pony", "Saddle", "Harness"];
+            return field;
+        case "Fisherman" :
+            var fisher = ["Fish Hook and Line", "Spear"];
+            return fisher;
+        case "Grave Robber" : 
+            var grave = ["Lantern", "Lamp Oil", "Pick", "Sack", "Spade"];
+            return grave;
+        case "Hedge Wizard" :
+            var hedge = ["Healing Draught", "Hood"];
+            return hedge;
+        case "Hunter" :
+            var hunt = ["Longbow", "10 Arrows", "2 Animal Traps", "Antitoxin Kit"];
+            return hunt;
+        case "Initiate" :
+            var init = ["Religious Symbol", "Robes"];
+            return init;
+        case "Jailer" :
+            var jail = ["Bottle of Common Wine", "Tankard", "Bola", "Lasso", "Net"];
+            return jail; 
+        case "Kislevite Kossar" :
+            var kislev = ["Bow", "10 Arrows", "Twohanded Axe", "Mail Coat", "Leather Jack", "Leather Leggings"];
+            return kislev;
+        case "Kithband Warrior" :
+            var kith = ["Elfbow", "10 Arrows", "Leather Jack"];
+            return kith;
+        case "Marine" :
+            var marine = ["Bow", "Crossbow", "10 Arrows", "10 Bolts", "Leather Jack", "Shield", "Grappling Hook", "10 Yards of Rope"];
+            return marine;
+        case "Mercenary" :
+            var merc = ["Crossbow", "10 Bolts", "Shield", "Mail Shirt", "Leather Jack", "Healing Draught"];
+            return merc;
+        case "Messenger" : 
+            var mess = ["Leather Jack", "Map Case", "Riding Horse", "Saddle", "Harness", "Shield"];
+            return mess;
+        case "Militiaman" :
+            var militia = ["Halberd", "Bow", "10 Arrows", "Leather Jack", "Leather Skullcap", "Uniform"];
+            return militia;
+        case "Miner" :
+            var miner = ["Two-handed Pick", "Leather Jack", "Pick, Spade", "Storm Lantern", "Lamp Oi"];
+            return miner;
+        case "Noble" :
+            var noble = ["Foil", "Main Gauche", "Noble’s Garb", "Riding Horse", "Saddle", "Harness", "10 Gold", "Jewellery worth 6d10 gc"];
+            return noble;
+        case "Norse Berserker" :
+            var norse = ["Leather Jerkin", "Bottle of Spirits", "Great Weapon", "Shield"];
+            return norse;
+        case "Outlaw" :
+            var outlaw = ["Bow", "10 Arrows", "Leather Jerkin", "Shield"];
+            return outlaw;
+        case "Outrider" :
+            var outrider = ["Bow", "Crossbow", "10 Arrows", "10 Bolts", "Net", "Whip", "Lasso","Leather Jack", "Shield", "10 Yards of Rope", "Riding Horse", "Saddle", "Harness"];
+            return outrider;
+        case "Peasant" :
+            var peasant = ["Sling", "Quarter Staff", "Leather Flask"];
+            return peasant; 
+        case "Pit Fighter" :
+            var pit = ["Flail",  "Great Weapon", "Knuckle-duster", "Shield", "Buckler", "Mail Shirt", "Leather Jack"];
+            return pit;
+        case "Protagonist" :
+            var protag = ["Mail Shirt",  "Leather Jack", "Shield", "Riding Horse", "Saddle", "Harness"];
+            return protag;
+        case "Rat Catcher" :
+            var rat = ["Sling with Ammunition", "4 Animal Traps", "Pole with 10 dead rats", "Small but Vicious Dog"];
+            return rat;
+        case "Roadwarden" :
+            var road = ["Pistol", "10 Firearm Balls and Gunpowder", "Mail Shirt", "Leather Jack", "Shield", "10 Yards of Rope", "Light Warhorse", "Saddle", "Harness"];
+            return road;
+        case "Rogue" :
+            var rogue = ["One set of Best Craftsmenship Clothing", "Dice", "Deck of Cards", "10 Gold"];
+            return rogue;
+        case "Runebearer" :
+            var rune = ["Crossbow", "10 Bolts", "Leather Jerkin", "Healing Draught", "Lucky Charm"];
+            return rune; 
+        case "Scribe" :
+            var scribe = ["Knife", "A Pair of Candles", "Wax", "5 matches", "Illuminated Book", "Writing Kit"];
+            return scribe; 
+        case "Seaman" :
+            var sea = ["Leather Jerkin", "Bottle of Poor Craftsmenship Spirits"];
+            return sea; 
+        case "Servant" :
+            var serve = ["One Set of Good Craftsmenship Clothing", "Pewter Tankard", "Tinderbox", "Storm Lantern", "Lamp Oil"];
+            return serve;
+        case "Shieldbreaker" :
+            var shield = ["Crossbow", "10 bolts", "Mail Coat", "Leather Jack", "Leather Leggings", "Shield", "Grappling Hook", "10 Yards of Rope", "Water Skin"];
+            return shield;
+        case "Smuggler" :
+            var smug = ["Leather Jack", "2 Torches", "Draft Horse", "Cart", "Rowing Boat"];
+            return smug;
+        case "Soldier" :
+            var soldider = ["Halberd",  "Firearm", "10 shots", "Shield" , "Full Leather Armour", "Uniform"]
+        case "Squire" :
+            var squire = ["Demilance", "Mail Shirt", "Mail Coif", "Leather Jack", "Shield", "Horse", "Saddle", "Harness"];
+            return squire; 
+        case "Student" :
+            var student = ["Two Textbooks corresponding to Knowledge Skills", "Writing Kit"];
+            return student;
+        case "Thief" :
+            var thief = ["Leather Jerkin", "Sack", "Lockpicks", "10 Yards of Rope"];
+            return thief;
+        case "Thug" :
+            var thug = ["Knuckle-dusters","Mail Shirt", "Leather Jerkin"];
+            return thug;
+        case "Toll Keeper" :
+            var toll = ["Chest", "Crossbow", "10 Bolts", "Mail Shirt", "Leather Jerkin", "Shield", "10 Gold"];
+            return toll;
+        case "Tomb Robber" :
+            var tomb = ["Leather Jack", "Crowbar", "Lantern", "Lamp Oil", "10 Yards of Rope", "2 Sacks"];
+            return tomb;
+        case  "Tradesman" :
+            var trades = ["Leather Jerkin", "10 Gold"];
+            return trades;
+        case "Troll Slayer" :
+            var troll = [ "Great Weapon", "Leather Jerkin", "One Bottle of Poor Craftsmenship Spirits"];
+            return troll;
+        case "Vagabond" :
+            var vaga = [ "Back Pack", "Rations (1 week)", "Tent", "Water Skin"];
+            return vaga;
+        case "Valet" :
+            var valet = [ "Cologne", "Purse", "Two sets of Best Craftsmenship Clothing", "Uniform"];
+            return valet;
+        case "Watchman" :
+            var watch = ["Leather Jack", "Lantern and Pole", "Lamp Oil", "Uniform"];
+            return watch; 
+        case "Woodsman" :
+            var woods = ["Great Weapon(Two-handed Axe)", "Leather Jack", "Antitoxin Kit"];
+            return woods; 
+        case "Zealot" :
+            var zealot = ["Flail", "Morning Star", "Leather Jack", "Bottle of Good Craftsmenship Spirits"];
+            return zealot;
+    }
 
 }
