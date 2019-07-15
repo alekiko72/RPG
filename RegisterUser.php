49a188f7dc1c75ae628b364955b7e53a810c4477
bshoupp <50969067+bshoupp@users.noreply.github.com>
2019-07-15 12:24:03 -0400

    Add files via upload

---
+++ RegisterUser.php
@@ -0,0 +1,41 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$username = $inData["username"];
+	$password = $inData["password"];
+	
+	$conn = new mysqli("localhost", "username", "password", "warhammer");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "INSERT into users (username, password) VALUES (" . $username . ",'" . $password . ") '";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
